7abf6c2e1126e9ae2fd157b658f98245ffd41b75
rahmadahyaWeb <thelastpc24@gmail.com>
2024-12-27 09:01:33 +0800

    add footer

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -186,6 +186,20 @@
                     </div>
                     <!-- / Content -->
 
+                    <footer class="content-footer footer bg-footer-theme">
+                        <div class="container-xxl">
+                            <div
+                                class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+                                <div class="text-body">
+                                    © {{ date('Y') }}, PT Kumala Central Partindo
+                                </div>
+                                <div class="text-body">
+                                    Designed & Developed by Rahmat
+                                </div>
+                            </div>
+                        </div>
+                    </footer>
+
                     <div class="content-backdrop fade"></div>
                 </div>
                 <!-- Content wrapper -->
