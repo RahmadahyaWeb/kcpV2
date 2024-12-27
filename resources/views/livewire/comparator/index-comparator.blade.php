40e0731c0a1aa09fbbab58a830f5934c9234453b
rahmadahyaWeb <thelastpc24@gmail.com>
2024-12-27 09:14:08 +0800

    perbaikan index comparator

--- resources/views/livewire/comparator/index-comparator.blade.php
+++ resources/views/livewire/comparator/index-comparator.blade.php
@@ -24,7 +24,7 @@
             </div>
 
             <div class="table-responsive">
-                <table class="table table-hover">
+                <table class="table">
                     <thead>
                         <tr>
                             <th>Part Number</th>
