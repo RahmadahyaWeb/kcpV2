85122211d8121fe1b266628138ae77724c47f4ab
rahmadahyaWeb <thelastpc24@gmail.com>
2025-01-08 16:18:20 +0800

    perbaikan

--- app/Http/Controllers/API/GoodsReceiptNONController.php
+++ app/Http/Controllers/API/GoodsReceiptNONController.php
@@ -212,6 +212,7 @@ class GoodsReceiptNONController extends Controller
      */
     public function sendDataToBosnet($data)
     {
+        dd($data);
         $credential = TokenBosnetController::signInForSecretKey();
 
         if (isset($credential['status'])) {
