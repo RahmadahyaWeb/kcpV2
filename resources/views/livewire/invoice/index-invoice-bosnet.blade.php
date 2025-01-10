c0bc32bda03f084ca9047b31abc23a3a40a833a4
rahmadahyaWeb <thelastpc24@gmail.com>
2025-01-10 14:43:58 +0800

    perbaikan

--- resources/views/livewire/invoice/index-invoice-bosnet.blade.php
+++ resources/views/livewire/invoice/index-invoice-bosnet.blade.php
@@ -9,8 +9,8 @@
                     Total Invoice {{ date('m-Y') }}
                 </div>
                 <div class="card-body">
-                    <span class="d-block fs-2 fw-bold" style="white-space: nowrap">
-                        Rp. {{ number_format($total_invoice, 0, ',', '.') }}
+                    <span class="d-block fs-2 fw-bold">
+                        {{ number_format($total_invoice, 0, ',', '.') }}
                     </span>
                 </div>
             </div>
