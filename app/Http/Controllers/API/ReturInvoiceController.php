ea78f2b75e499127daabc9ea685976cdb352f44f
rahmadahyaWeb <thelastpc24@gmail.com>
2025-01-13 15:43:20 +0800

    perbaikan

--- app/Http/Controllers/API/ReturInvoiceController.php
+++ app/Http/Controllers/API/ReturInvoiceController.php
@@ -153,6 +153,7 @@ class ReturInvoiceController extends Controller
      */
     private function sendDataToBosnet($data)
     {
+        dd($data);
         $credential = TokenBosnetController::signInForSecretKey();
 
         if (isset($credential['status'])) {
