fe47fd69b9acbae35a7ea528947ba5d38c34db81
rahmadahyaWeb <thelastpc24@gmail.com>
2025-01-14 11:20:54 +0800

    perbaikan

--- app/Http/Controllers/API/LogController.php
+++ app/Http/Controllers/API/LogController.php
@@ -10,12 +10,18 @@ class LogController extends Controller
 {
     public static function log_api($dataToSend, $response, $status = false)
     {
+        if ($status) {
+            $status = 'SUKSES';
+        } else {
+            $status = 'GAGAL';
+        }
+
         DB::table('log_api')
             ->insert([
                 'date'      => now(),
                 'request'   => json_encode($dataToSend),
                 'response'  => json_encode($response),
-                'status'    => "$status"
+                'status'    => $status
             ]);
     }
 }
