13a6afa1f795b9e581ed52109fd32948ced3e062
rahmadahyaWeb <thelastpc24@gmail.com>
2025-01-28 10:56:17 +0800

    perbaikan

--- app/Exports/InvoiceAopExport.php
+++ app/Exports/InvoiceAopExport.php
@@ -2,15 +2,70 @@
 
 namespace App\Exports;
 
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class InvoiceAopExport implements FromCollection
+class InvoiceAopExport implements FromCollection, WithHeadings, WithMapping
 {
+    protected $from_date;
+    protected $to_date;
+
+    public function __construct($from_date, $to_date)
+    {
+        $this->from_date = $from_date;
+        $this->to_date = $to_date;
+    }
+
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
-        //
+        return DB::table('invoice_aop_header')
+            ->select([
+                'tanggalJatuhTempo',
+                'invoiceAop',
+                'billingDocumentDate',
+                'price',
+                'addDiscount',
+                'extraPlafonDiscount',
+                'amount',
+                'tax',
+                'grandTotal'
+            ])
+            ->whereBetween('billingDocumentDate', [$this->from_date, $this->to_date])
+            ->get();
+    }
+
+    public function map($row): array
+    {
+        return [
+            $row->tanggalJatuhTempo,
+            $row->invoiceAop,
+            $row->billingDocumentDate,
+            $row->price,
+            $row->addDiscount,
+            $row->extraPlafonDiscount,
+            $row->amount,
+            $row->tax,
+            $row->grandTotal,
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'TANGGAL JATUH TEMPO',
+            'NO PEMBELIAN',
+            'TANGGAL',
+            'HARGA',
+            'DISC',
+            'EXTRA PLAFON DISC',
+            'DPP',
+            'TAX',
+            'GRAND TOTAL',
+        ];
     }
 }
--- app/Livewire/ReportFinance/PurchaseAopReport.php
+++ app/Livewire/ReportFinance/PurchaseAopReport.php
@@ -8,18 +8,23 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class PurchaseAopReport extends Component
 {
-    public $target = '';
+    public $target = 'export_to_excel';
     public $from_date;
     public $to_date;
 
     public function export_to_excel()
     {
+        $this->validate([
+            'from_date' => 'required',
+            'to_date' => 'required',
+        ]);
+
         $fromDateFormatted = \Carbon\Carbon::parse($this->from_date)->format('Ymd');
         $toDateFormatted = \Carbon\Carbon::parse($this->to_date)->format('Ymd');
 
         $filename = "invoice_aop{$fromDateFormatted}_-_{$toDateFormatted}.xlsx";
 
-        return Excel::download(new InvoiceAopExport($this->fromDate, $this->toDate), $filename);
+        return Excel::download(new InvoiceAopExport($this->from_date, $this->to_date), $filename);
     }
 
     public function render()
