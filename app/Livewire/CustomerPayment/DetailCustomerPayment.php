e72f6f085360d39875bb9f74c66c88e7e66fb64e
rahmadahyaWeb <thelastpc24@gmail.com>
2025-01-31 09:23:45 +0800

    perbaikan

--- app/Livewire/CustomerPayment/DetailCustomerPayment.php
+++ app/Livewire/CustomerPayment/DetailCustomerPayment.php
@@ -114,41 +114,50 @@ class DetailCustomerPayment extends Component
                 $kcpapplication->beginTransaction();
                 $kcpinformation->beginTransaction();
 
-                $kcpinformation->table('trns_pembayaran_piutang_header')
-                    ->insert([
-                        'nopiutang'         => $this->customer_payment_header->no_piutang,
-                        'area_piutang'      => $this->customer_payment_header->area_piutang,
-                        'kd_outlet'         => $this->customer_payment_header->kd_outlet,
-                        'nm_outlet'         => $this->customer_payment_header->nm_outlet,
-                        'nominal_potong'    => $this->customer_payment_header->nominal_potong,
-                        'pembayaran_via'    => $this->customer_payment_header->pembayaran_via,
-                        'no_bg'             => $this->customer_payment_header->no_bg,
-                        'jth_tempo_bg'      => $this->customer_payment_header->tgl_jth_tempo_bg,
-                        'bank'              => $this->customer_payment_details[0]->bank,
-                        'status'            => 'C',
-                        'no_bg'             => $this->customer_payment_header->no_bg,
-                        'crea_date'         => $this->customer_payment_header->crea_date,
-                        'crea_by'           => $this->customer_payment_header->crea_by,
-                    ]);
+                $cek_no_piutang_kcpinformation = $kcpinformation->table('trns_pembayaran_piutang_header')
+                    ->where('nopiutang', $this->customer_payment_header->no_piutang)
+                    ->first();
+
+                if (!$cek_no_piutang_kcpinformation) {
+                    $kcpinformation->table('trns_pembayaran_piutang_header')
+                        ->insert([
+                            'nopiutang'         => $this->customer_payment_header->no_piutang,
+                            'area_piutang'      => $this->customer_payment_header->area_piutang,
+                            'kd_outlet'         => $this->customer_payment_header->kd_outlet,
+                            'nm_outlet'         => $this->customer_payment_header->nm_outlet,
+                            'nominal_potong'    => $this->customer_payment_header->nominal_potong,
+                            'pembayaran_via'    => $this->customer_payment_header->pembayaran_via,
+                            'no_bg'             => $this->customer_payment_header->no_bg,
+                            'jth_tempo_bg'      => $this->customer_payment_header->tgl_jth_tempo_bg,
+                            'bank'              => $this->customer_payment_details[0]->bank,
+                            'status'            => 'C',
+                            'no_bg'             => $this->customer_payment_header->no_bg,
+                            'crea_date'         => $this->customer_payment_header->crea_date,
+                            'crea_by'           => $this->customer_payment_header->crea_by,
+                            'modi_date'         => $this->customer_payment_header->crea_date,
+                        ]);
+                }
 
                 foreach ($this->customer_payment_details as $value) {
                     $no_invoice = self::formatInvoiceNumber($value->noinv);
 
-                    $kcpinformation->table('trns_pembayaran_piutang')
-                        ->insert([
-                            'noinv'             => $no_invoice,
-                            'nopiutang'         => $value->no_piutang,
-                            'kd_outlet'         => $value->kd_outlet,
-                            'nm_outlet'         => $value->nm_outlet,
-                            'nominal'           => $value->nominal,
-                            'keterangan'        => $value->keterangan,
-                            'pembayaran_via'    => $value->pembayaran_via,
-                            'no_bg'             => $value->no_bg,
-                            'jth_tempo_bg'      => $value->tgl_jth_tempo_bg,
-                            'status'            => 'C',
-                            'crea_date'         => $value->crea_date,
-                            'crea_by'           => $value->crea_by,
-                        ]);
+                    if (!$cek_no_piutang_kcpinformation) {
+                        $kcpinformation->table('trns_pembayaran_piutang')
+                            ->insert([
+                                'noinv'             => $no_invoice,
+                                'nopiutang'         => $value->no_piutang,
+                                'kd_outlet'         => $value->kd_outlet,
+                                'nm_outlet'         => $value->nm_outlet,
+                                'nominal'           => $value->nominal,
+                                'keterangan'        => $value->keterangan,
+                                'pembayaran_via'    => $value->pembayaran_via,
+                                'no_bg'             => $value->no_bg,
+                                'jth_tempo_bg'      => $value->tgl_jth_tempo_bg,
+                                'status'            => 'C',
+                                'crea_date'         => $value->crea_date,
+                                'crea_by'           => $value->crea_by,
+                            ]);
+                    }
 
                     // FLAG PEMBAYARAN LUNAS
                     $paymentSummary = DB::connection('kcpinformation')->table('trns_pembayaran_piutang AS pay')
