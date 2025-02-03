c615fe01fbaa4daeb0a37ed614954b4da0b049c7
rahmadahyaWeb <thelastpc24@gmail.com>
2025-02-03 10:49:57 +0800

    perbaikan

--- app/Livewire/ProductPart.php
+++ app/Livewire/ProductPart.php
@@ -8,6 +8,22 @@ use Livewire\Component;
 class ProductPart extends Component
 {
     public $data_aop, $data_non_aop;
+    public $periode;
+
+    public function mount()
+    {
+        $this->periode = date('Y-m');
+
+        $this->data_aop = [
+            'labels' => $this->fetch_product_aop(),
+            'amount' => $this->fetch_amount_aop()
+        ];
+
+        $this->data_non_aop = [
+            'labels' => $this->fetch_product_non_aop(),
+            'amount' => $this->fetch_amount_non_aop()
+        ];
+    }
 
     private function fetch_product_by_supplier(array $suppliers)
     {
@@ -35,7 +51,7 @@ class ProductPart extends Component
         return $product_non_aop;
     }
 
-    private function fetch_amount_by_supplier(array $suppliers)
+    private function fetch_amount_by_supplier(array $suppliers, $periode)
     {
         $kcpinformation = DB::connection('kcpinformation');
 
@@ -54,7 +70,7 @@ class ProductPart extends Component
                         'part_info.produk_part',
                         DB::raw('SUM(inv_detail.nominal_total) as amount_total')
                     ])
-                    ->whereRaw("SUBSTR(inv.crea_date, 1, 7) = 2025-01")
+                    ->whereRaw("SUBSTR(inv.crea_date, 1, 7) = ?", [date('Y-m', strtotime($periode))])
                     ->where('inv.flag_batal', 'N')
                     ->groupBy('part_info.produk_part'),
                 'invoice_data',
@@ -70,18 +86,18 @@ class ProductPart extends Component
 
     public function fetch_amount_aop()
     {
-        $amount_aop = $this->fetch_amount_by_supplier(['ASTRA OTOPART']);
+        $amount_aop = $this->fetch_amount_by_supplier(['ASTRA OTOPART'], $this->periode);
 
         return $amount_aop;
     }
 
     public function fetch_amount_non_aop()
     {
-        $amounts = $this->fetch_amount_by_supplier(['KMC', 'ABM', 'SSI']);
+        $amounts = $this->fetch_amount_by_supplier(['KMC', 'ABM', 'SSI'], $this->periode);
         return $amounts;
     }
 
-    public function render()
+    public function updatedPeriode()
     {
         $this->data_aop = [
             'labels' => $this->fetch_product_aop(),
@@ -92,7 +108,10 @@ class ProductPart extends Component
             'labels' => $this->fetch_product_non_aop(),
             'amount' => $this->fetch_amount_non_aop()
         ];
+    }
 
+    public function render()
+    {
         return view('livewire.product-part');
     }
 }
--- resources/views/livewire/product-part.blade.php
+++ resources/views/livewire/product-part.blade.php
@@ -1,29 +1,52 @@
-<div x-data="{
-    dataAop: @entangle('data_aop'),
-    dataNonAop: @entangle('data_non_aop')
-}" x-init="$nextTick(() => {
-    initializeChart('product_aop', dataAop, 'Penjualan Produk AOP', 'rgba(54, 162, 235, 0.6)', 'rgba(54, 162, 235, 1)');
-    initializeChart('product_non_aop', dataNonAop, 'Penjualan Produk Non-AOP', 'rgba(255, 99, 132, 0.6)', 'rgba(255, 99, 132, 1)');
-})">
+<div>
 
     <x-dashboard-navigation />
 
-    <div class="row gap-3">
-        <div class="col-12">
-            <div class="chartCard">
-                <div class="chartBox">
-                    <div class="canvas">
-                        <canvas id="product_aop"></canvas>
+    <div class="row mb-3">
+        <div class="col-md-4">
+            <label class="form-label" for="periode">Periode</label>
+            <input type="month" id="periode" class="form-control" wire:model.change="periode">
+        </div>
+    </div>
+
+    <div x-data="{
+        dataAop: @entangle('data_aop'),
+    }" x-init="$nextTick(() => {
+        initializeChart('product_aop', dataAop, 'Penjualan Produk AOP', 'rgba(255, 99, 132, 0.6)', 'rgba(255, 99, 132, 1)');
+    })"
+        x-effect="$watch('dataAop', () => {
+        initializeChart('product_aop', dataAop, 'Penjualan Produk AOP', 'rgba(255, 99, 132, 0.6)', 'rgba(255, 99, 132, 1)');
+    })">
+
+        <div class="row gap-3 mb-3">
+            <div class="col-12">
+                <div class="chartCard">
+                    <div class="chartBox">
+                        <div class="canvas">
+                            <canvas id="product_aop"></canvas>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="col-12">
-            <div class="chartCard">
-                <div class="chartBox">
-                    <div class="canvas">
-                        <canvas id="product_non_aop"></canvas>
+    <div x-data="{
+        dataNonAop: @entangle('data_non_aop'),
+    }" x-init="$nextTick(() => {
+        initializeChart('product_non_aop', dataNonAop, 'Penjualan Produk AOP', 'rgba(54, 162, 235, 0.6)', 'rgba(54, 162, 235, 1)');
+    })"
+        x-effect="$watch('dataNonAop', () => {
+        initializeChart('product_non_aop', dataNonAop, 'Penjualan Produk AOP', 'rgba(54, 162, 235, 0.6)', 'rgba(54, 162, 235, 1)');
+    })">
+
+        <div class="row gap-3 mb-3">
+            <div class="col-12">
+                <div class="chartCard">
+                    <div class="chartBox">
+                        <div class="canvas">
+                            <canvas id="product_non_aop"></canvas>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -33,11 +56,22 @@
     @push('script')
         <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.7/dist/chart.umd.min.js"></script>
         <script>
+            let productAopChart = null;
+            let productNonAopChart = null;
+
             // Fungsi untuk menginisialisasi chart
             function initializeChart(canvasId, data, label, bgColor, borderColor) {
-                console.log(`${label}:`, data.amount);
+                console.log("Data for chart:", data); // Debugging data
 
                 const ctx = document.getElementById(canvasId);
+
+                // Hancurkan chart lama jika sudah ada
+                if (canvasId === 'product_aop' && productAopChart) {
+                    productAopChart.destroy();
+                } else if (canvasId === 'product_non_aop' && productNonAopChart) {
+                    productNonAopChart.destroy();
+                }
+
                 const labels = data.labels;
 
                 const chartData = {
@@ -62,11 +96,20 @@
                     }
                 };
 
-                new Chart(ctx, {
-                    type: 'bar',
-                    data: chartData,
-                    options: chartOptions
-                });
+                // Buat chart baru dan simpan objek chart ke dalam variabel
+                if (canvasId === 'product_aop') {
+                    productAopChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: chartData,
+                        options: chartOptions
+                    });
+                } else if (canvasId === 'product_non_aop') {
+                    productNonAopChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: chartData,
+                        options: chartOptions
+                    });
+                }
             }
 
             // Fungsi untuk membuat dataset
