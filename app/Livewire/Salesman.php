7754296fa67743bb68586850582a19d75527c34c
rahmadahyaWeb <thelastpc24@gmail.com>
2025-02-03 15:18:49 +0800

    test

--- app/Livewire/Salesman.php
+++ app/Livewire/Salesman.php
@@ -7,42 +7,58 @@ use Livewire\Component;
 
 class Salesman extends Component
 {
-    public function fetch_salesman()
+    public $target = 'periode';
+    public $data_salesman;
+    public $periode;
+
+    public function mount()
     {
-        $kcpinformation = DB::connection('kcpinformation');
-
-        $salesman = $kcpinformation->table('user')
-            ->where('role', 'SALESMAN')
-            ->where('status', 'Y')
-            ->pluck('username');
-
-        return $salesman;
+        $this->periode = date('Y-m', strtotime('2024-12'));
     }
 
     public function fetch_invoice_salesman()
     {
         $kcpinformation = DB::connection('kcpinformation');
 
-        $currentMonth = date('Y-m', strtotime('2025-01'));
+        $periode = $this->periode;
 
         $invoice = $kcpinformation->table('user as salesman')
-            ->leftJoin('trns_inv_header as invoice', function ($join) use ($currentMonth) {
+            ->leftJoin('trns_inv_header as invoice', function ($join) use ($periode) {
                 $join->on('salesman.username', '=', 'invoice.user_sales')
-                    ->whereRaw("SUBSTR(invoice.crea_date, 1, 7) = ?", [$currentMonth]);
+                    ->whereRaw("SUBSTR(invoice.crea_date, 1, 7) = ?", [$periode])
+                    ->where('invoice.flag_batal', '<>', 'Y');
             })
-            ->where('invoice.flag_batal', '<>', 'Y')
+            ->leftJoin('trns_retur_header as retur_header', function ($join) use ($periode) {
+                $join->on('retur_header.noinv', '=', 'invoice.noinv')
+                    ->where('retur_header.flag_nota', '=', 'Y')
+                    ->whereRaw("SUBSTR(retur_header.flag_nota_date, 1, 7) = ?", [$periode]);
+            })
+            ->leftJoin('trns_retur_details as retur_details', 'retur_header.noretur', '=', 'retur_details.noretur')
             ->where('salesman.role', 'SALESMAN')
             ->where('salesman.status', 'Y')
-            ->select('salesman.fullname as user_sales', DB::raw('COALESCE(SUM(invoice.amount_total), 0) as total_amount'))
-            ->groupBy('salesman.username')
+            ->select(
+                'salesman.username as user_sales',
+                'salesman.fullname',
+                DB::raw('COALESCE(SUM(invoice.amount_total), 0) as total_amount'),
+                DB::raw('COALESCE(SUM(CASE WHEN retur_header.noinv IS NOT NULL THEN retur_details.nominal_total ELSE 0 END), 0) as total_retur')
+            )
+            ->groupBy('salesman.username', 'salesman.fullname')
+            ->orderBy('total_amount', 'desc')
             ->get();
 
-        dd($invoice);
+        return $invoice;
     }
+
 
     public function render()
     {
-        dd($this->fetch_invoice_salesman());
-        return view('livewire.salesman');
+        $salesmanData = $this->fetch_invoice_salesman();
+
+        $this->data_salesman = [
+            'labels' => $salesmanData->pluck('fullname'),
+            'amount' => $salesmanData->pluck('total_amount'),
+        ];
+
+        return view('livewire.salesman', compact('salesmanData'));
     }
 }
--- resources/views/livewire/salesman.blade.php
+++ resources/views/livewire/salesman.blade.php
@@ -1,3 +1,134 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+    <x-loading :target="$target" />
+
+    <x-dashboard-navigation />
+
+    <div class="row mb-3">
+        <div class="col-md-4">
+            <label class="form-label" for="periode">Periode</label>
+            <input type="month" id="periode" class="form-control" wire:model.change="periode">
+        </div>
+    </div>
+
+    <div class="row mb-3">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>NO</th>
+                                    <th>NAMA SALES</th>
+                                    <th>AMOUNT TOTAL (INVOICE)</th>
+                                    <th>AMOUNT TOTAL (RETUR)</th>
+                                    <th>TOTAL</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php
+                                    $no = 1;
+                                @endphp
+
+                                @foreach ($salesmanData as $sales)
+                                    <tr>
+                                        <td>{{ $no++ }}</td>
+                                        <td>{{ $sales->fullname }}</td>
+                                        <td>{{ number_format($sales->total_amount, 0, ',', '.') }}</td>
+                                        <td>{{ number_format($sales->total_retur, 0, ',', '.') }}</td>
+                                        <td>{{ number_format($sales->total_amount - $sales->total_retur, 0, ',', '.') }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div x-data="{
+        dataSalesman: @entangle('data_salesman'),
+    }" x-init="$nextTick(() => {
+        initializeChart('salesman', dataSalesman, 'Total Invoice Terbentuk', 'rgba(255, 99, 132, 0.6)', 'rgba(255, 99, 132, 1)');
+    })"
+        x-effect="$watch('dataSalesman', () => {
+        initializeChart('salesman', dataSalesman, 'Total Invoice Terbentuk', 'rgba(255, 99, 132, 0.6)', 'rgba(255, 99, 132, 1)');
+    })">
+
+        <div class="row gap-3 mb-3">
+            <div class="col-12">
+                <div class="chartCard">
+                    <div class="chartBox">
+                        <div class="canvas">
+                            <canvas id="salesman"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @push('script')
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.7/dist/chart.umd.min.js"></script>
+        <script>
+            // Fungsi untuk menginisialisasi chart
+            function initializeChart(canvasId, data, label, bgColor, borderColor) {
+
+                const ctx = document.getElementById(canvasId);
+
+                // Clear the canvas content
+                ctx.getContext('2d').clearRect(0, 0, ctx.width, ctx.height);
+
+                // Destroy the previous chart instance if it exists
+                if (window[canvasId + 'Chart']) {
+                    window[canvasId + 'Chart'].destroy();
+                }
+
+                const labels = data.labels;
+
+                const chartData = {
+                    labels: labels,
+                    datasets: [
+                        createDataset(label, bgColor, borderColor, data.amount),
+                    ]
+                };
+
+                const chartOptions = {
+                    maintainAspectRatio: false,
+                    plugins: {
+                        legend: {
+                            display: true,
+                            position: 'top'
+                        }
+                    },
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                };
+
+                // Create the new chart and store the chart instance in a global variable
+                if (canvasId === 'salesman') {
+                    window['salesmanChart'] = new Chart(ctx, {
+                        type: 'bar',
+                        data: chartData,
+                        options: chartOptions
+                    });
+                }
+            }
+
+            // Fungsi untuk membuat dataset
+            function createDataset(label, backgroundColor, borderColor, data) {
+                return {
+                    label: label,
+                    data: data,
+                    backgroundColor: backgroundColor,
+                    borderColor: borderColor,
+                    borderWidth: 1
+                };
+            }
+        </script>
+    @endpush
 </div>
