5b91d78aa6def8648b06d9f570f03c580c20c67c
rahmadahyaWeb <thelastpc24@gmail.com>
2025-02-04 16:14:21 +0800

    perbaikan

--- resources/views/components/loading.blade.php
+++ resources/views/components/loading.blade.php
@@ -7,17 +7,3 @@
         <span class="visually-hidden">Loading...</span>
     </div>
 </div>
-
-<div wire:offline>
-    <div class="d-flex flex-column text-center justify-content-center align-items-center"
-        style="position: fixed; top: 0; left: 0; width: 100%; height: 100%;
-    background-color: rgba(0, 0, 0, 0.1); z-index: 9999;">
-        <div class="card">
-            <div class="card-body">
-                <span class="mb-1 fs-2 fw-bold text-uppercase">
-                    Anda sedang offline
-                </span>
-            </div>
-        </div>
-    </div>
-</div>
