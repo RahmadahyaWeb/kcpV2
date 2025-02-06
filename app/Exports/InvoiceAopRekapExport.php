a7858d9607b6ff8554b5aedd25abdd2c9567e142
rahmadahyaWeb <thelastpc24@gmail.com>
2025-02-06 09:10:28 +0800

    perbaikan

--- app/Exports/InvoiceAopRekapExport.php
+++ app/Exports/InvoiceAopRekapExport.php
@@ -6,8 +6,10 @@ use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class InvoiceAopRekapExport implements FromCollection, WithHeadings, WithMapping
+class InvoiceAopRekapExport implements FromCollection, WithHeadings, WithMapping, WithColumnFormatting
 {
     protected $from_date;
     protected $to_date;
@@ -61,6 +63,13 @@ class InvoiceAopRekapExport implements FromCollection, WithHeadings, WithMapping
             'PART NO',
             'QTY',
             'AMOUNT'
+        ];
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'C' => NumberFormat::FORMAT_DATE_DDMMYYYY,
         ];
     }
 }
