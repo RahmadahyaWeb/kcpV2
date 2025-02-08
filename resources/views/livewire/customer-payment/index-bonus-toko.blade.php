e8f0fec23606fe6a026e1ea5318e8bdee38971f3
rahmadahyaWeb <thelastpc24@gmail.com>
2025-02-08 10:05:31 +0800

    penambahan

--- resources/views/livewire/customer-payment/index-bonus-toko.blade.php
+++ resources/views/livewire/customer-payment/index-bonus-toko.blade.php
@@ -15,6 +15,7 @@
                             <th>NO INVOICE</th>
                             <th>KODE TOKO</th>
                             <th>NAMA TOKO</th>
+                            <th>NOMINAL</th>
                             <th>NOMINAL DISC</th>
                             <th>NOMINAL TOTAL</th>
                             <th></th>
@@ -26,6 +27,7 @@
                                 <td class="text-nowrap">{{ $item->noinv }}</td>
                                 <td class="text-nowrap">{{ $item->kd_outlet }}</td>
                                 <td class="text-nowrap">{{ $item->nm_outlet }}</td>
+                                <td class="text-nowrap">{{ number_format($item->amount, 0, ',', '.') }}</td>
                                 <td class="text-nowrap">{{ number_format($item->amount_disc, 0, ',', '.') }}</td>
                                 <td class="text-nowrap">{{ number_format($item->amount_total, 0, ',', '.') }}</td>
                                 <td>
@@ -37,7 +39,7 @@
                             </tr>
                         @empty
                             <tr>
-                                <td colspan="6" class="text-center">No Data</td>
+                                <td colspan="7" class="text-center">No Data</td>
                             </tr>
                         @endforelse
                     </tbody>
