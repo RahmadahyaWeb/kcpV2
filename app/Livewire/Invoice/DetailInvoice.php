e18460c24690781edd31dc82e4ce14ad07e16e68
rahmadahyaWeb <thelastpc24@gmail.com>
2025-02-11 13:54:01 +0800

    perbaikan

--- app/Livewire/Invoice/DetailInvoice.php
+++ app/Livewire/Invoice/DetailInvoice.php
@@ -183,14 +183,18 @@ class DetailInvoice extends Component
      */
     public function sendToBosnet()
     {
-        // session()->flash('error', 'FITUR SEDANG DIPERBAIKI, SEMENTARA BELUM BISA DIPAKAI, TERIMA KASIH.');
-        try {
-            $controller = new SalesOrderController();
-            $controller->sendToBosnet(new Request(['invoice' => $this->invoice]));
+        $user = Auth::user();
+        if ($user->hasRole('super-user')) {
+            try {
+                $controller = new SalesOrderController();
+                $controller->sendToBosnet(new Request(['invoice' => $this->invoice]));
 
-            session()->flash('success', "Data SO berhasil diteruskan ke BOSNET");
-        } catch (\Exception $e) {
-            session()->flash('error', $e->getMessage());
+                session()->flash('success', "Data SO berhasil diteruskan ke BOSNET");
+            } catch (\Exception $e) {
+                session()->flash('error', $e->getMessage());
+            }
+        } else {
+            session()->flash('error', 'FITUR SEDANG DIPERBAIKI, SEMENTARA BELUM BISA DIPAKAI, TERIMA KASIH.');
         }
     }
 
