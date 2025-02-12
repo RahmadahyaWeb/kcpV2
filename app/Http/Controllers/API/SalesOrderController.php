05f9d8830988199f7f47bc76b41283db5b02f4ff
rahmadahyaWeb <thelastpc24@gmail.com>
2025-02-12 11:23:23 +0800

    perbaikan

--- app/Http/Controllers/API/SalesOrderController.php
+++ app/Http/Controllers/API/SalesOrderController.php
@@ -255,7 +255,6 @@ class SalesOrderController extends Controller
      */
     private function sendDataToBosnet($data)
     {
-        dd($data);
         $credential = TokenBosnetController::signInForSecretKey();
 
         if (isset($credential['status'])) {
