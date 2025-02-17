f167de0700fe38d23f55c6b85b2b2332355c00ba
rahmadahyaWeb <thelastpc24@gmail.com>
2025-02-17 10:45:00 +0800

    perbaikan

--- app/Http/Controllers/API/ReturInvoiceController.php
+++ app/Http/Controllers/API/ReturInvoiceController.php
@@ -150,6 +150,7 @@ class ReturInvoiceController extends Controller
      */
     private function sendDataToBosnet($data)
     {
+        dd($data);
         $credential = TokenBosnetController::signInForSecretKey();
 
         if (isset($credential['status'])) {
@@ -249,18 +250,18 @@ class ReturInvoiceController extends Controller
 
         // Loop through each retur item and calculate the amounts
         foreach ($returItems as $returItem) {
-            // $decTax = round(((($returItem->nominal_total / $returItem->qty) * $returItem->qty) / config('tax.ppn_factor')) * config('tax.ppn_percentage'));
-            // $decAmount = ($returItem->nominal_total / $returItem->qty) * $returItem->qty;
-            // $decDPP = round((($returItem->nominal_total / $returItem->qty) * $returItem->qty) / config('tax.ppn_factor'));
-            // $decPrice = $returItem->nominal_total / $returItem->qty;
+            $decTax = round(((($returItem->nominal_total / $returItem->qty) * $returItem->qty) / config('tax.ppn_factor')) * config('tax.ppn_percentage'));
+            $decAmount = ($returItem->nominal_total / $returItem->qty) * $returItem->qty;
+            $decDPP = round((($returItem->nominal_total / $returItem->qty) * $returItem->qty) / config('tax.ppn_factor'));
+            $decPrice = $returItem->nominal_total / $returItem->qty;
 
-            $decPrice = $returItem->hrg_pcs;
-            $qty = $returItem->qty;
-            $decDisc =  $returItem->nominal_disc;
-            $decDiscPerItem = $decDisc / $qty;
-            $decAmount = $decPrice * $qty;
-            $decDPP = round(($decAmount - $decDisc) / config('tax.ppn_factor'));
-            $decTax = round($decDPP * config('tax.ppn_percentage'));
+            // $decPrice = $returItem->hrg_pcs;
+            // $qty = $returItem->qty;
+            // $decDisc =  $returItem->nominal_disc;
+            // $decDiscPerItem = $decDisc / $qty;
+            // $decAmount = $decPrice * $qty;
+            // $decDPP = round(($decAmount - $decDisc) / config('tax.ppn_factor'));
+            // $decTax = round($decDPP * config('tax.ppn_percentage'));
 
             // Update totals
             $decDPPTotal += $decDPP;
@@ -273,7 +274,8 @@ class ReturInvoiceController extends Controller
                 'decQty'             => -$returItem->qty,
                 'szUomId'            => "PCS",
                 'decPrice'           => $decPrice,
-                'decDiscount'        => -$decDisc,
+                // 'decDiscount'        => -$decDisc,
+                'decDiscount'        => -0,
                 'bTaxable'           => true,
                 'decTax'             => -$decTax,
                 'decAmount'          => -$decAmount,
