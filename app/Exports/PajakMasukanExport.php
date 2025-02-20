bc04e0ae2ebe5274b92889a922f44a142e8a0b0c
rahmadahyaWeb <thelastpc24@gmail.com>
2025-02-20 10:34:43 +0800

    perbaikan

---
+++ app/Exports/PajakMasukanExport.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Exports;
+
+use DateTime;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+
+class PajakMasukanExport implements FromCollection, WithTitle, WithMapping, WithHeadings, WithColumnFormatting, WithEvents
+{
+    private $from_date, $to_date;
+
+    public function __construct($from_date, $to_date)
+    {
+        $this->from_date = $from_date;
+        $this->to_date = $to_date;
+    }
+
+    public function title(): string
+    {
+        return "FakturPajakMasukan" . $this->from_date . "-" . $this->to_date;
+    }
+
+    public function map($row): array
+    {
+        $fm = "FM";
+        $kd_jenis_transaksi = "1";
+        $fg_pengganti = "0";
+        $nomor_faktur = $row->fakturPajak;
+        $tanggal_faktur = $row->billingDocumentDate;
+        $masa_pajak = date('m', strtotime($tanggal_faktur));
+        $tahun_pajak = date('Y', strtotime($tanggal_faktur));
+        $tanggal_faktur_excel = Date::dateTimeToExcel(new DateTime($tanggal_faktur));
+        $npwp = "013452438054000";
+        $nama = "PT ASTRA OTOPARTS TBK.";
+        $alamat_lengkap = "";
+        $jumlah_dpp = $row->netSales;
+        $jumlah_ppn = $row->tax;
+        $jumlah_ppnb = 0;
+        $is_creditable = 1;
+
+        return [
+            $fm,
+            $kd_jenis_transaksi,
+            $fg_pengganti,
+            $nomor_faktur,
+            $masa_pajak,
+            $tahun_pajak,
+            $tanggal_faktur_excel,
+            $npwp,
+            $nama,
+            $alamat_lengkap,
+            $jumlah_dpp,
+            $jumlah_ppn,
+            $jumlah_ppnb,
+            $is_creditable
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'FM',
+            'KD_JENIS_TRANSAKSI',
+            'FG_PENGGANTI',
+            'NOMOR_FAKTUR',
+            'MASA_PAJAK',
+            'TAHUN_PAJAK',
+            'TANGGAL_FAKTUR',
+            'NPWP',
+            'NAMA',
+            'ALAMAT_LENGKAP',
+            'JUMLAH_DPP',
+            'JUMLAH_PPN',
+            'JUMLAH_PPNB',
+            'IS_CREDITABLE'
+        ];
+    }
+
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $sheet = $event->sheet;
+
+                $highestRow = $sheet->getHighestRow();
+                for ($row = 1; $row <= $highestRow; $row++) {
+                    $sheet->getDelegate()->setCellValueExplicit('C' . $row, $sheet->getCell('C' . $row)->getValue(), \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
+                    $sheet->getDelegate()->setCellValueExplicit('D' . $row, $sheet->getCell('D' . $row)->getValue(), \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
+                    $sheet->getDelegate()->setCellValueExplicit('H' . $row, $sheet->getCell('H' . $row)->getValue(), \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
+                    $sheet->getDelegate()->setCellValueExplicit('M' . $row, $sheet->getCell('M' . $row)->getValue(), \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
+                    $sheet->getDelegate()->setCellValueExplicit('N' . $row, $sheet->getCell('N' . $row)->getValue(), \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
+                }
+            }
+        ];
+    }
+
+    public function collection()
+    {
+        return DB::table('invoice_aop_header as header')
+            // ->join('invoice_aop_detail as detail', 'detail.invoiceAop', '=', 'header.invoiceAop')
+            ->whereBetween('header.billingDocumentDate', [$this->from_date, $this->to_date])
+            ->select([
+                'invoiceAop',
+                'fakturPajak',
+                'billingDocumentDate',
+                'netSales',
+                'tax'
+            ])
+            ->get();
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'G' => NumberFormat::FORMAT_DATE_DDMMYYYY,
+        ];
+    }
+}
--- app/Livewire/Pajak/IndexPajakMasukan.php
+++ app/Livewire/Pajak/IndexPajakMasukan.php
@@ -2,12 +2,29 @@
 
 namespace App\Livewire\Pajak;
 
+use App\Exports\PajakMasukanExport;
 use Livewire\Component;
+use Maatwebsite\Excel\Facades\Excel;
 
 class IndexPajakMasukan extends Component
 {
     public $target = '';
     public $from_date, $to_date;
+
+    public function export_to_excel()
+    {
+        $this->validate([
+            'from_date'     => ['required'],
+            'to_date'       => ['required'],
+        ]);
+
+        $fromDateFormatted = \Carbon\Carbon::parse($this->from_date)->startOfDay();
+        $toDateFormatted = \Carbon\Carbon::parse($this->to_date)->endOfDay();
+
+        $filename = "pajak_masukan" . $fromDateFormatted . "_" . $toDateFormatted . ".xlsx";
+
+        return Excel::download(new PajakMasukanExport($fromDateFormatted, $toDateFormatted), $filename);
+    }
 
     public function render()
     {
