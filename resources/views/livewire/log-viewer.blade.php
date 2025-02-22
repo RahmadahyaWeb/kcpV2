9e402f75261965dc36debe8622ae2923a4407435
rahmadahyaWeb <thelastpc24@gmail.com>
2025-02-22 11:16:39 +0800

    perbaikan

--- resources/views/livewire/log-viewer.blade.php
+++ resources/views/livewire/log-viewer.blade.php
@@ -41,7 +41,7 @@
                                 <td style="white-space: nowrap">{{ $item->date }}</td>
                                 <td @click="copyText($event, '{{ addslashes($item->request) }}')">
                                     {{ substr($item->request, 0, 100) }}...</td>
-                                <td>{{ substr($item->response, 0, 300) }}...</td>
+                                <td @click="copyText($event, '{{ addslashes($item->response) }}')">{{ substr($item->response, 0, 300) }}...</td>
                                 <td>
                                     <span class="badge text-bg-{{ $item->status == 'SUKSES' ? 'success' : 'danger' }}">
                                         @if ($item->status == 'SUKSES')
