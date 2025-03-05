a90cc5c6a5ee6a8301c880b2351cfbf6b66a32fd
rahmadahyaWeb <thelastpc24@gmail.com>
2025-03-05 21:04:12 +0800

    perbaikan

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -206,10 +206,11 @@ class SyncController extends Controller
         // Ambil data dari tabel invoice_aop_header
         $invoice_aop = $kcpapplication->table('invoice_aop_header')
             ->whereDate('billingDocumentDate', '>=', '2025-02-28')
-            ->select('SPB', 'customerTo')
+            ->select('SPB', 'customerTo', 'invoiceAop')
             ->orderBy('created_at', 'desc')
-            ->groupBy('SPB', 'customerTo')
             ->get();
+
+        dd($invoice_aop);
 
         // Ambil data dari tabel intransit_header
         // $intransit_aop = $kcpinformation->table('intransit_header')
@@ -235,13 +236,12 @@ class SyncController extends Controller
             try {
                 $kcpinformation->beginTransaction();
 
-                $no_sp_aop = "$value->SPB" . "$value->customerTo";
+                $spb = $value->SPB;
                 $kd_gudang_aop = $value->customerTo;
-
-                dd($no_sp_aop);
+                $no_sp_aop = "$spb" . "$kd_gudang_aop";
 
                 $invoice_aop_details = $kcpapplication->table('invoice_aop_detail')
-                    ->where('SPB', $value->SPB)
+                    ->where('SPB', $spb)
                     ->get();
 
                 // Mengambil data nm_part dari database 'kcpinformation'
