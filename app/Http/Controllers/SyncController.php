2d94ab9fed84df445b45f5a272560e8f38634047
rahmadahyaWeb <thelastpc24@gmail.com>
2025-03-05 20:50:57 +0800

    perbaikan

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -67,9 +67,8 @@ class SyncController extends Controller
 
         // Ambil data dari tabel invoice_aop_header
         $invoice_aop = $kcpapplication->table('invoice_aop_header')
-            ->whereDate('billingDocumentDate', '<>', '2025-02-28')
-            ->orderBy('created_at', 'desc')
             ->select('SPB', 'customerTo')
+            ->orderBy('created_at', 'desc')
             ->groupBy('SPB', 'customerTo')
             ->get();
 
