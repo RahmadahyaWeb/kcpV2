2b3540ee5afcec75fe3d01acef2453172bce9138
rahmadahyaWeb <thelastpc24@gmail.com>
2025-03-05 18:33:08 +0800

    perbaikan

--- app/Livewire/Intransit/DetailIntransit.php
+++ app/Livewire/Intransit/DetailIntransit.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Intransit;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
@@ -30,7 +31,42 @@ class DetailIntransit extends Component
             ->whereIn('id', $this->selectedItems)
             ->get();
 
-        dd($items);
+        try {
+            $kcpinformation->beginTransaction();
+
+            foreach ($items as $item) {
+                if ($item->qty == $item->qty_terima && $item->status == 'I' && $item->kd_rak <> '') {
+
+                    // DATA MASTER PART
+                    $data_master_part = $kcpinformation->table('mst_part')
+                        ->where('part_no', $item->part_no)
+                        ->first();
+
+                    if (!$data_master_part) {
+                        $kcpinformation->table('mst_part')
+                            ->insert([
+                                'part_no' => $item->part_no,
+                                'status' => 'Y',
+                                'crea_date' => now(),
+                                'crea_by' => Auth::user()->username,
+                            ]);
+                    }
+
+                    // STOCK PART
+                    $data_stock_part = $kcpinformation->table('mst_gudang')
+                        ->join('stock_part', 'stock_part.kd_gudang', '=', 'mst_gudang.kd_gudang')
+                        ->where('mst_gudang.kd_gudang_aop', $item->kd_gudang_aop)
+                        ->where('stock_part.part_no', $item->part_no)
+                        ->where('stock_part.status', 'A')
+                        ->get();
+
+                    dd($data_stock_part);
+                }
+            }
+        } catch (\Exception $e) {
+            $kcpinformation->rollBack();
+            session()->flash('error', $e->getMessage());
+        }
     }
 
     public function render()
