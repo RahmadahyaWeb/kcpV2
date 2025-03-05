7a3d272b90a51ecd24c54042a8fb332a3ed66dc5
rahmadahyaWeb <thelastpc24@gmail.com>
2025-03-05 20:53:24 +0800

    perbaikan

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -67,13 +67,10 @@ class SyncController extends Controller
 
         // Ambil data dari tabel invoice_aop_header
         $invoice_aop = $kcpapplication->table('invoice_aop_header')
-            ->where('SPB', '8700001234')
             ->select('SPB', 'customerTo')
             ->orderBy('created_at', 'desc')
             ->groupBy('SPB', 'customerTo')
             ->get();
-
-        dd($invoice_aop);
 
         // Ambil data dari tabel intransit_header
         $intransit_aop = $kcpinformation->table('intransit_header')
