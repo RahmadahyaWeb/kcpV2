681c6f2ec66c03f8d0fba2e15a0a00efd9671ed6
rahmadahyaWeb <thelastpc24@gmail.com>
2025-03-05 17:19:37 +0800

    perbaikan

--- app/Livewire/Intransit/IndexIntransit.php
+++ app/Livewire/Intransit/IndexIntransit.php
@@ -2,12 +2,29 @@
 
 namespace App\Livewire\Intransit;
 
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class IndexIntransit extends Component
 {
+    use WithPagination;
+
+    public $target = '';
+
     public function render()
     {
-        return view('livewire.intransit.index-intransit');
+        $kcpinformation = DB::connection('kcpinformation');
+
+        $items = $kcpinformation->table('intransit_header')
+            ->select([
+                'delivery_note'
+            ])
+            ->where('status', 'I')
+            ->whereDate('tgl_packingsheet', '>', '2017-07-01')
+            ->groupBy('delivery_note')
+            ->get();
+
+        return view('livewire.intransit.index-intransit', compact('items'));
     }
 }
--- resources/views/livewire/intransit/index-intransit.blade.php
+++ resources/views/livewire/intransit/index-intransit.blade.php
@@ -1,3 +1,39 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <x-alert />
+    <x-loading :target="$target" />
+
+    <div class="card">
+        <div class="card-header">
+            Data Intransit
+        </div>
+
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Delivery Note</th>
+                            <th>Kode Gudang</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($items as $item)
+                            <tr>
+                                <td class="text-nowrap">{{ $item->delivery_note }}</td>
+                                <td>{{ $item->kd_gudang_aop }}</td>
+                                <td>
+                                    <a href="" class="btn btn-primary">Detail</a>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="3" class="text-center">No Data</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
