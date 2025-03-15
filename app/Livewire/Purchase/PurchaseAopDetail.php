cba78e87a0d8104f8492a2eb62577bc8eefc63e0
rahmadahyaWeb <thelastpc24@gmail.com>
2025-03-15 11:44:48 +0800

    testing

--- app/Livewire/Purchase/PurchaseAopDetail.php
+++ app/Livewire/Purchase/PurchaseAopDetail.php
@@ -54,16 +54,16 @@ class PurchaseAopDetail extends Component
 
     public function sendToBosnet()
     {
-        session()->flash('error', 'fitur ditutup sementara');
+        // session()->flash('error', 'fitur ditutup sementara');
 
-        // try {
-        //     $controller = new PurchaseOrderAOPController();
-        //     $controller->sendToBosnet(new Request(['invoiceAop' => $this->invoiceAop]));
+        try {
+            $controller = new PurchaseOrderAOPController();
+            $controller->sendToBosnet(new Request(['invoiceAop' => $this->invoiceAop]));
 
-        //     session()->flash('success', "Data PO berhasil dikirim!");
-        // } catch (\Exception $e) {
-        //     session()->flash('error', $e->getMessage());
-        // }
+            session()->flash('success', "Data PO berhasil dikirim!");
+        } catch (\Exception $e) {
+            session()->flash('error', $e->getMessage());
+        }
     }
 
     public function openModalFakturPajak()
