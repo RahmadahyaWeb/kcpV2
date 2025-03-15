f52c0f665f83f37871a03061b5547c179c0e737d
rahmadahyaWeb <thelastpc24@gmail.com>
2025-03-15 12:20:42 +0800

    perbaikan

--- app/Livewire/Intransit/IndexIntransit.php
+++ app/Livewire/Intransit/IndexIntransit.php
@@ -27,13 +27,13 @@ class IndexIntransit extends Component
         //     ->whereDate('crea_date', '>=', '2025-03-05')
         //     ->delete();
 
-        $item = $kcpinformation->table('intransit_details')
-            ->where('no_sp_aop', '8700010672KCP02001')
-            // ->where('part_no', 'H2-412PA-K18-110H')
-            ->update([
-                'no_sp_aop' => "8700010671KCP02001",
-                'delivery_note' => "8700010671KCP02001"
-            ]);
+        // $item = $kcpinformation->table('intransit_details')
+        //     ->where('no_sp_aop', '8700010672KCP02001')
+        //     // ->where('part_no', 'H2-412PA-K18-110H')
+        //     ->update([
+        //         'no_sp_aop' => "8700010671KCP02001",
+        //         'delivery_note' => "8700010671KCP02001"
+        //     ]);
 
 
         $items = $kcpinformation->table('intransit_header')
