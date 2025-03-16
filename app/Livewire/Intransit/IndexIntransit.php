f65d198c14fbb0ea56176d2c4e5e2bb699469d2b
rahmadahyaWeb <thelastpc24@gmail.com>
2025-03-16 10:59:11 +0800

    perbaikan

--- app/Livewire/Intransit/IndexIntransit.php
+++ app/Livewire/Intransit/IndexIntransit.php
@@ -17,15 +17,15 @@ class IndexIntransit extends Component
     {
         $kcpinformation = DB::connection('kcpinformation');
 
-        // $header_to_rollback = $kcpinformation->table('intransit_header')
-        //     ->where('crea_by', 'SYSTEM')
-        //     ->whereDate('tgl_packingsheet', '>=', '2025-03-05')
-        //     ->delete();
+        $header_to_rollback = $kcpinformation->table('intransit_header')
+            ->where('crea_by', 'SYSTEM')
+            ->whereDate('tgl_packingsheet', '>=', '2025-03-05')
+            ->delete();
 
-        // $details_to_rollback = $kcpinformation->table('intransit_details')
-        //     ->where('crea_by', 'SYSTEM')
-        //     ->whereDate('crea_date', '>=', '2025-03-05')
-        //     ->delete();
+        $details_to_rollback = $kcpinformation->table('intransit_details')
+            ->where('crea_by', 'SYSTEM')
+            ->whereDate('crea_date', '>=', '2025-03-05')
+            ->delete();
 
         // $item = $kcpinformation->table('intransit_details')
         //     ->where('no_sp_aop', '8700010672KCP02001')
