57181765c3244b7f3300158dc7110918af6074ad
rahmadahyaWeb <thelastpc24@gmail.com>
2025-03-20 14:03:24 +0800

    perbaikan

--- app/Livewire/GoodsReceipt/GoodsReceiptAopDetail.php
+++ app/Livewire/GoodsReceipt/GoodsReceiptAopDetail.php
@@ -146,6 +146,8 @@ class GoodsReceiptAopDetail extends Component
 
         $this->items_with_qty = $items_with_qty;
 
+        dd($items_with_qty);
+
         return view('livewire.goods-receipt.goods-receipt-aop-detail', compact(
             'items_with_qty',
             'spb',
