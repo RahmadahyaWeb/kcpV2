61d3a207db46b317c2413e2cd2bbffd9aae9ea54
rahmadahyaWeb <thelastpc24@gmail.com>
2025-03-20 11:54:56 +0800

    perbaikan

--- app/Livewire/Intransit/FormUpdateIntransit.php
+++ app/Livewire/Intransit/FormUpdateIntransit.php
@@ -45,7 +45,7 @@ class FormUpdateIntransit extends Component
 
         session()->flash('success', "Berhasil update part number");
 
-        $this->redirectRoute('intransit.detail', $this->delivery_note);
+        $this->redirectRoute('intransit.detail', $this->no_sp_aop);
     }
 
     public function render()
