124919d3e4057f435a999919f7d252e2e012e864
rahmadahyaWeb <thelastpc24@gmail.com>
2025-04-03 16:07:06 +0800

    perbaikan

--- app/Livewire/StockPart/IndexStockPart.php
+++ app/Livewire/StockPart/IndexStockPart.php
@@ -34,26 +34,27 @@ class IndexStockPart extends Component
         // Ambil daftar part_no untuk query kedua
         $partNumbers = $items->pluck('part_no')->toArray();
 
-        // Ambil data intransit stock
         $intransitStock = $kcpinformation->table('intransit_details as intransit')
             ->join('mst_gudang as gudang', 'intransit.kd_gudang_aop', '=', 'gudang.kd_gudang_aop')
             ->where('intransit.status', 'I')
             ->whereIn('intransit.part_no', $partNumbers)
-            ->groupBy('intransit.kd_gudang_aop', 'intransit.part_no')
+            ->groupBy('intransit.kd_gudang_aop', 'intransit.part_no', 'gudang.kd_gudang') // Tambahkan kd_gudang
             ->select(
                 'intransit.kd_gudang_aop as gudang_aop',
+                'gudang.kd_gudang', // Tambahkan kd_gudang
                 'intransit.part_no',
                 DB::raw('SUM(intransit.qty) as qty_intransit')
             )
             ->get();
 
-        // Buat mapping untuk qty_intransit
-        $intransitMap = $intransitStock->keyBy('part_no');
+        $intransitMap = $intransitStock->mapWithKeys(function ($intransit) {
+            $key = $intransit->part_no . '-' . $intransit->kd_gudang; // Gunakan key unik
+            return [$key => $intransit];
+        });
 
-        // Tambahkan qty_intransit ke hasil utama
         $modifiedItems = $items->map(function ($item) use ($intransitMap) {
-            $partNo = $item->part_no;
-            $item->qty_intransit = $intransitMap[$partNo]->qty_intransit ?? 0;
+            $key = $item->part_no . '-' . $item->kd_gudang; // Sesuaikan dengan key yang sudah dibuat
+            $item->qty_intransit = $intransitMap[$key]->qty_intransit ?? 0;
             return $item;
         });
 
