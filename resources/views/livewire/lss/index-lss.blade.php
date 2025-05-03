715f1d4c9937dac9ed125386f9caef4e342fc310
rahmadahyaWeb <thelastpc24@gmail.com>
2025-05-03 10:35:24 +0800

    perbaikan

--- resources/views/livewire/lss/index-lss.blade.php
+++ resources/views/livewire/lss/index-lss.blade.php
@@ -19,6 +19,16 @@
                                 <select class="form-select @error('bulan') is-invalid @enderror" wire:model="bulan">
                                     <option value="01">Januari</option>
                                     <option value="02">Februari</option>
+                                    <option value="03">Maret</option>
+                                    <option value="04">April</option>
+                                    <option value="05">Mei</option>
+                                    <option value="06">Juni</option>
+                                    <option value="07">Juli</option>
+                                    <option value="08">Agustus</option>
+                                    <option value="09">September</option>
+                                    <option value="10">Oktober</option>
+                                    <option value="11">November</option>
+                                    <option value="12">Desember</option>
                                 </select>
 
                                 @error('bulan')
