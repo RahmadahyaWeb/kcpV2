273883b63d9c2aea572e1efddd15070875b79504
rahmadahyaWeb <thelastpc24@gmail.com>
2025-05-09 16:18:15 +0800

    perbaikan

--- app/Livewire/ReportFinance/AgingReport.php
+++ app/Livewire/ReportFinance/AgingReport.php
@@ -169,10 +169,7 @@ class AgingReport extends Component
                     $result[$kd_outlet]['retur']['invoice_numbers'][] = $invoice->noinv;
 
                     // Kurangi total_piutang dengan nilai retur (sudah negatif, cukup dijumlahkan)
-                    $result[$kd_outlet]['total_piutang'] += $invoice->remaining_balance;
-
-                    // Sesuaikan sisa limit kredit dengan mengurangi nilai retur
-                    // $result[$kd_outlet]['sisa_limit_kredit'] += $invoice->remaining_balance;
+                    // $result[$kd_outlet]['total_piutang'] += $invoice->remaining_balance;
 
                     continue;
                 }
